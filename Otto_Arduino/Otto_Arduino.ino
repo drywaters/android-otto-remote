ca6678e82fe7193a530cb9a2eba5b6ff6af073e6
Daniel Waters <danwater1@gmail.com>
2016-12-11 08:27:02 -0800

    Final Code!

--- Otto_Arduino/Otto_Arduino.ino
+++ Otto_Arduino/Otto_Arduino.ino
@@ -1,132 +1,118 @@
-/*const int BUZZER_PIN = 13;
+#include <Servo.h>
+#include "Oscillator.h"
+#include <EEPROM.h>
 
+#define N_SERVOS 4
+//-- First step: Configure the pins where the servos are attached
+/*
+         ---------------
+        |     O   O     |
+        |---------------|
+YR 3==> |               | <== YL 2
+         ---------------
+            ||     ||
+            ||     ||
+RR 5==>   -----   ------  <== RL 4
+         |-----   ------|
+*/
+#define TRIM_RR 7
+#define TRIM_RL 4
+#define TRIM_YR 4
+#define TRIM_YL -7
+//OTTO.setTrims(-7,-4,-4,7);
+
+#define PIN_RR 5
+#define PIN_RL 4
+#define PIN_YR 3
+#define PIN_YL 2
+
+#define C 2100
+#define D 1870
+#define E 1670
+#define f 1580
+#define G 1400
+#define R 0
+
+#define INTERVALTIME 10.0
+
+Oscillator servo[N_SERVOS];
+
+const int trigPin = 9;
+const int echoPin = 8;
+const int speakerOut = 10;
+
+// function prototypes
+void turnLeft(int steps, int T=3000);
+void turnRight(int steps, int T=3000);
+void walk(int steps, int T=1000);
+void backyard(int steps, int T=3000);
+bool safeDistance();
+void playMusic();
+
+int melody[] = {E,E,E,R,E,E,E,R,E,G,C,D,E,R,f,f,f,f,f,E,E,E,E,D,D,E,D};
+int MAX_COUNT = sizeof(melody) / 2; // Melody length, for looping.
+// Set overall tempo
+long tempo = 7500;
+// Set length of pause between notes
+int pause = 1000;
+// Loop variable to increase Rest length
+int rest_count = 70;
+// Initialize core variables
+int tone_ = 0;
+int beat = 0;
+long duration  = 0;
 
 void setup() {
   // put your setup code here, to run once:
 
-    Serial.begin(115200);
-    
-    
-    
+  Serial.begin(115200);
 
+  servo[0].attach(PIN_RR);
+  servo[1].attach(PIN_RL);
+  servo[2].attach(PIN_YR);
+  servo[3].attach(PIN_YL);
+
+  servo[0].SetTrim(TRIM_RR);
+  servo[1].SetTrim(TRIM_RL);
+  servo[2].SetTrim(TRIM_YR);
+  servo[3].SetTrim(TRIM_YL);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(speakerOut, OUTPUT);
+
+  for(int i=0;i<4;i++) servo[i].SetPosition(90);
 }
+
+// TEMPO: 121 BPM
+int t=495;
 
 void loop() {
 
-
-  
-
-  
   // put your main code here, to run repeatedly:
   if(Serial.available()) {
     char character = Serial.read();
     switch(character) {
-      case 'U':
+      case 'F':
         Serial.println("MOVING OTTO UP");
+        if (safeDistance()) {
+          walk(2, t*2);
+        } else {
+          tone(10, 440, 1000);
+        }
         break;
-      case 'D':
+      case 'B':
         Serial.println("MOVING OTTO DOWN");
+        tone(13, 800, 100);
+        back(2, t*2);
         break;
       case 'L':
         Serial.println("MOVING OTTO LEFT");
+        turnLeft(2, t*2);
         break;
       case 'R':
         Serial.println("MOVING OTTO RIGHT");
-        break;
-    }
-  }
-}
-*/
-
-#include "note_definition.h"
-
-int speakerOut = 9;
-bool played = false;
-
-long melody[] = { E5, E5, E5, E5, E5, E5, E5, G5, C5, D5, E5, F5, F5, F5, F5, F5, E5, E5, E5, E5, D5, D5, E5, D5, G5, E5, E5, E5, E5, E5, E5, E5, G5, C5, D5, E5, F5, F5, F5, F5, F5, E5, E5, E5, G5, G5, F5, D5, C5 };
-int beats[]  =  {  8,  8, 16,  8,  8, 16,  8,  8, 12,  4, 32,  8,  8, 12,  4,  8,  8,  8,  8,  8,  8,  8,  8, 16, 16,  8,  8, 16,  8,  8, 16,  8,  8, 12,  4, 32,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 32 }; 
-
-int MAX_COUNT = sizeof(melody) / sizeof(long); // Melody length, for looping.
-
-// Set overall tempo
-long tempo = 50000;
-
-// Set length of pause between notes
-long pause = 100000;
-
-// Loop variable to increase Rest length
-int rest_count = 100; 
-
-// Initialize core variables
-long tone_ = 0;
-int beat = 0;
-long duration  = 0;
-
-
-void playMusic() {
-  
-  for (int i=0; i<MAX_COUNT; i++) {
-   
-    tone_ = melody[i];
-    beat = beats[i];
-
-    duration = beat * tempo; // Set up timing
-
-    playTone(); 
-    // A pause between notes...
-    delayMicroseconds(pause);
-    }
-  
-}
-
-void playTone() {
-  long elapsed_time = 0;
-  if (tone_ > 0) { // if this isn't a Rest beat, while the tone has 
-    //  played less long than 'duration', pulse speaker HIGH and LOW
-    while (elapsed_time < duration) {
-
-      digitalWrite(speakerOut,HIGH);
-      delayMicroseconds(tone_ / 2);
-
-      // DOWN
-      digitalWrite(speakerOut, LOW);
-      delayMicroseconds(tone_ / 2);
-
-      // Keep track of how long we pulsed
-      elapsed_time += (tone_);
-    } 
-  }
-  else { // Rest beat; loop times delay
-    for (int j = 0; j < rest_count; j++) { // See NOTE on rest_count
-      delayMicroseconds(duration);  
-    }                                
-  }                                 
-}
-
-void setup() { 
-  
-  pinMode(speakerOut, OUTPUT);
-
-  Serial.begin(115200);
-  
-}
-
-void loop() {
-
-  if(Serial.available()) {
-    char character = Serial.read();
-    switch(character) {
-      case 'U':
-        Serial.println("MOVING OTTO UP");
-        break;
-      case 'D':
-        Serial.println("MOVING OTTO DOWN");
-        break;
-      case 'L':
-        Serial.println("MOVING OTTO LEFT");
-        break;
-      case 'R':
-        Serial.println("MOVING OTTO RIGHT");
+        turnRight(2, t*2);
         break;
       case 'M':
         Serial.println("PLAYING MUSIC");
@@ -134,4 +120,102 @@ void loop() {
         break;
     }
   }
+}
+
+void oscillate(int A[N_SERVOS], int O[N_SERVOS], int T, double phase_diff[N_SERVOS]){
+  for (int i=0; i<4; i++) {
+    servo[i].SetO(O[i]);
+    servo[i].SetA(A[i]);
+    servo[i].SetT(T);
+    servo[i].SetPh(phase_diff[i]);
+  }
+  double ref=millis();
+   for (double x=ref; x<T+ref; x=millis()){
+     for (int i=0; i<4; i++){
+        servo[i].refresh();
+     }
+  }
+}
+
+void back(int steps, int T){
+    int A[4]= {15, 15, 30, 30};
+    int O[4] = {0, 0, 0, 0};
+    double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)};
+
+    for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
+}
+
+void walk(int steps, int T){
+    int A[4]= {15, 15, 30, 30};
+    int O[4] = {0, 0, 0, 0};
+    double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(-90), DEG2RAD(-90)};
+
+    for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
+}
+
+void turnLeft(int steps, int T){
+    int A[4]= {20, 20, 10, 30};
+    int O[4] = {0, 0, 0, 0};
+    double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)};
+
+    for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
+}
+
+void turnRight(int steps, int T){
+    int A[4]= {20, 20, 30, 10};
+    int O[4] = {0, 0, 0, 0};
+    double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)};
+
+    for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
+}
+
+bool safeDistance() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Make sure not sending any sound
+  delayMicroseconds(2); // Clears the speaker for 2 microseconds
+  digitalWrite(trigPin, HIGH);  // Start sound waves
+  delayMicroseconds(10); // Run for 10 microseconds
+  digitalWrite(trigPin, LOW); // Turn off sound waves
+  duration = pulseIn(echoPin, HIGH);  // Set the echo pin to wait till receive 5V
+  distance = (duration/2) * 0.03435;  // Calculate the distance by formula (Distance = Speed * Time)
+                                      // Time / 2 because do not want the time from the object and back
+                                      // We only want the time to the object.
+                                      // 0.03435 is the speed of sound in CM/S^2
+  return (distance > 5);
+}
+
+void playTone()
+{
+   long elapsed_time = 0;
+   if(tone_ > 0) { // if this isn't a Rest beat, while the tone has
+     //  played less long than 'duration', pulse speaker HIGH and LOW
+   while(elapsed_time < duration) {
+       digitalWrite(speakerOut,HIGH);
+       delayMicroseconds(tone_ / 2);
+       // DOWN
+       digitalWrite(speakerOut, LOW);
+       delayMicroseconds(tone_ / 2);
+       // Keep track of how long we pulsed
+       elapsed_time +=(tone_);
+     }
+   }
+   else
+   {
+    // Rest beat; loop times delay
+     for(int j = 0; j < rest_count; j++)
+   {
+       delayMicroseconds(duration);
+     }
+   }
+}
+
+void playMusic() {
+    for(int i=0; i < MAX_COUNT; i++) {
+    tone_ = melody[i];
+    beat = 50;
+    duration = beat * tempo; // Set up timing
+    playTone();
+    // A pause between notes...
+    delayMicroseconds(pause);
+    }
 }
--- app/src/main/java/com/watersjournal/ottoremote/OttoRemote.java
+++ app/src/main/java/com/watersjournal/ottoremote/OttoRemote.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ScrollView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class OttoRemote extends BlunoLibrary {
@@ -14,38 +14,105 @@ public class OttoRemote extends BlunoLibrary {
     private Button buttonSerialSend;
     private EditText serialSendText;
     private TextView serialReceivedText;
+    private ImageButton buttonUp;
+    private ImageButton buttonDown;
+    private ImageButton buttonLeft;
+    private ImageButton buttonRight;
+    private ImageButton buttonMusic;
+    private long startTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        startTime = System.currentTimeMillis();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otto_remote);
-        onCreateProcess();														//onCreate Process by BlunoLibrary
 
+        onCreateProcess();	//onCreate Process by BlunoLibrary
 
-        serialBegin(115200);													//set the Uart Baudrate on BLE chip to 115200
+        serialBegin(115200);	//set the Uart Baudrate on BLE chip to 115200
 
-      /*  serialReceivedText=(TextView) findViewById(R.id.serialReveicedText);	//initial the EditText of the received data
-        serialSendText=(EditText) findViewById(R.id.serialSendText);			//initial the EditText of the sending data
-
-        buttonSerialSend = (Button) findViewById(R.id.buttonSerialSend);		//initial the button for sending the data
-        buttonSerialSend.setOnClickListener(new OnClickListener() {
-
+        // Button to move Otto forward
+        buttonUp = (ImageButton) findViewById(R.id.upButton);
+        buttonUp.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-
-                serialSend(serialSendText.getText().toString());				//send the data to the BLUNO
+            public void onClick(View view) {
+                long diff = System.currentTimeMillis() - startTime;
+                // Only send the command if the last one was over a second ago
+                // To allow Otto to finish the movement
+                if (System.currentTimeMillis() > startTime + 1000) {
+                    serialSend("F");
+                    startTime = System.currentTimeMillis();
+                }
             }
         });
-*/
-        buttonScan = (Button) findViewById(R.id.buttonScan);					//initial the button for scanning the BLE device
+
+        // Button to move Otto backward
+        buttonDown = (ImageButton) findViewById(R.id.downButton);
+        buttonDown.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long diff = System.currentTimeMillis() - startTime;
+                // Only send the command if the last one was over a second ago
+                // To allow Otto to finish the movement
+                if (System.currentTimeMillis() > startTime + 1000) {
+                    serialSend("B");
+                    startTime = System.currentTimeMillis();
+                }
+            }
+        });
+
+        // Button to move Otto left
+        buttonLeft = (ImageButton) findViewById(R.id.leftButton);
+        buttonLeft.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long diff = System.currentTimeMillis() - startTime;
+                // Only send the command if the last one was over a second ago
+                // To allow Otto to finish the movement
+                if (System.currentTimeMillis() > startTime + 1000) {
+                    serialSend("L");
+                    startTime = System.currentTimeMillis();
+                }
+            }
+        });
+
+        // Button to move Otto right
+        buttonRight = (ImageButton) findViewById(R.id.rightButton);
+        buttonRight.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long diff = System.currentTimeMillis() - startTime;
+                // Only send the command if the last one was over a second ago
+                // To allow Otto to finish the movement
+                if (System.currentTimeMillis() > startTime + 1000) {
+                    serialSend("R");
+                    startTime = System.currentTimeMillis();
+                }
+            }
+        });
+
+        // Button to have Otto play some music
+        buttonMusic = (ImageButton) findViewById(R.id.musicButton);
+        buttonMusic.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long diff = System.currentTimeMillis() - startTime;
+                if (System.currentTimeMillis() > startTime + 1000) {
+                    serialSend("M");
+                    startTime = System.currentTimeMillis();
+                }
+            }
+        });
+
+
+        //initial the button for scanning the BLE device
+        buttonScan = (Button) findViewById(R.id.buttonScan);
         buttonScan.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-
-                buttonScanOnClickProcess();										//Alert Dialog for selecting the BLE device
+                buttonScanOnClickProcess();		//Alert Dialog for selecting the BLE device
             }
         });
     }
@@ -53,37 +120,38 @@ public class OttoRemote extends BlunoLibrary {
     protected void onResume(){
         super.onResume();
         System.out.println("BlUNOActivity onResume");
-        onResumeProcess();														//onResume Process by BlunoLibrary
+        onResumeProcess();					//onResume Process by BlunoLibrary
     }
 
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        onActivityResultProcess(requestCode, resultCode, data);					//onActivityResult Process by BlunoLibrary
+        onActivityResultProcess(requestCode, resultCode, data);		//onActivityResult Process by BlunoLibrary
         super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        onPauseProcess();														//onPause Process by BlunoLibrary
+        onPauseProcess();				//onPause Process by BlunoLibrary
     }
 
     protected void onStop() {
         super.onStop();
-        onStopProcess();														//onStop Process by BlunoLibrary
+        onStopProcess();				//onStop Process by BlunoLibrary
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        onDestroyProcess();														//onDestroy Process by BlunoLibrary
+        onDestroyProcess();				//onDestroy Process by BlunoLibrary
     }
 
+    //Once connection state changes, this function will be called
     @Override
-    public void onConectionStateChange(connectionStateEnum theConnectionState) {//Once connection state changes, this function will be called
-        switch (theConnectionState) {											//Four connection state
+    public void onConectionStateChange(connectionStateEnum theConnectionState) {
+        switch (theConnectionState) {			//Four connection states
             case isConnected:
                 buttonScan.setText("Connected");
                 break;
@@ -104,12 +172,10 @@ public class OttoRemote extends BlunoLibrary {
         }
     }
 
+    //Once connection data received, this function will be called
     @Override
-    public void onSerialReceived(String theString) {							//Once connection data received, this function will be called
-        // TODO Auto-generated method stub
-        serialReceivedText.append(theString);							//append the text into the EditText
-        //The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
-        ((ScrollView)serialReceivedText.getParent()).fullScroll(View.FOCUS_DOWN);
+    public void onSerialReceived(String theString) {
+
     }
 
 }
